6afaf8ab5e13a0547cf22ae275435fbfb482971e
Owen <owen.dodd@gmail.com>
2018-09-13 20:46:08 -0400

    finalize slideshow

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,35 @@
-
-
 $(function() {
-  var first_slide = $('.slider>.slide').first();
+  var   first_slide = $('.slider>.slide').first(),
+        total_slides =$('.slider>.slide').length;
 
   first_slide.addClass('show');
-})
+  $('#indicator').html('1 / ' + total_slides);
+
 
 $('.next').click(function () {
-  var total_slides = $('.slider.slide').length,
-    current_slide = $('.slider>.slide.show'),
-    current_position = $(current_slide).index() + 1,
-
-    first_slide = $('.slider>.slide').first(),
-    slide_plus = $(current_slide).next(),
-
-    next_slide = current_position == total_slides ? first_slide : slide_plus;
+  var   current_slide = $('.slider>.slide.show'), 
+        total_slides =$('.slider>.slide').length,
+        should_loop = current_slide.next().length > 0,
+        next_slide = should_loop ? current_slide.next() : $('.slider>.slide:first');
 
   current_slide.removeClass('show');
   next_slide.addClass('show');
 
+  $('#indicator').html((next_slide.index() + 1) + ' / ' + total_slides);
+
 })
 
 $('.prev').click(function () {
-  var total_slides = $('.slider>.slide').length,
-    current_slide = $('.slider>.slide.show'),
-    current_position = $(current_slide).index() + 1,
-
-    last_slide = $('.slider>.slide').last(),
-    slide_minus = $(current_slide).prev(),
-
-    prev_slide = current_position == 1 ? last_slide : slide_minus;
+  var   current_slide = $('.slider>.slide.show'), 
+        total_slides =$('.slider>.slide').length,
+        should_loop = current_slide.prev().length > 0,
+        prev_slide = should_loop ? current_slide.prev() : $('.slider>.slide:last');
 
   current_slide.removeClass('show');
   prev_slide.addClass('show');
+  
+  $('#indicator').html((prev_slide.index() + 1) + ' / ' + total_slides);
 
-})
+});
+
+});
