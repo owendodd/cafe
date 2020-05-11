5f07f595d82381d126f831382a38f2447c2c2c28
owendodd <owendodd@users.noreply.github.com>
2020-05-11 14:12:58 +0000

    deploy: 2dd106523e5c873a9bddccefab53cb27226aa8c2

--- js/script.js
+++ js/script.js
@@ -1,144 +1,120 @@
 $(function () {
-  var first_slide = $('.slider>.slide:first-of-type'),
-      total_slides = $('.slider>.slide').length;
+  var first_slide = $(".slider>.slide:first-of-type"),
+    total_slides = $(".slider>.slide").length;
 
-  first_slide.addClass('show');
-  $('.indicator').html('1 / ' + total_slides);
+  first_slide.addClass("show");
+  $(".indicator").html("1 / " + total_slides);
 
+  const sliders = document.querySelectorAll(".slider");
 
-  $('.next').click(function () {
-    var slider = $(this).closest('.content').children('.slider'),
-      current_slide = slider.children('.slide.show'),
-      total_slides = slider.children('.slide').length,
+  for (let index = 0; index < sliders.length; index++) {
+    const element = sliders[index];
+    if (!element.innerHTML.trim()) {
+      element.parentElement.remove();
+    }
+  }
+
+  $(".next").click(function () {
+    var slider = $(this).closest(".content").children(".slider"),
+      current_slide = slider.children(".slide.show"),
+      total_slides = slider.children(".slide").length,
       should_loop = current_slide.next().length > 0,
-      next_slide = should_loop ? current_slide.next() : slider.children('.slide:first');
+      next_slide = should_loop
+        ? current_slide.next()
+        : slider.children(".slide:first");
 
-    current_slide.removeClass('show');
-    next_slide.addClass('show');
+    current_slide.removeClass("show");
+    next_slide.addClass("show");
 
-    $('#indicator').html((next_slide.index() + 1) + ' / ' + total_slides);
-
-    console.log(total_slides);
-  
-
+    $("#indicator").html(next_slide.index() + 1 + " / " + total_slides);
   });
 
-
-  $('.prev').click(function () {
-    var slider = $(this).closest('.content').children('.slider'),
-      current_slide = slider.children('.slide.show'),
-      total_slides = slider.children('.slide').length,
+  $(".prev").click(function () {
+    var slider = $(this).closest(".content").children(".slider"),
+      current_slide = slider.children(".slide.show"),
+      total_slides = slider.children(".slide").length,
       should_loop = current_slide.prev().length > 0,
-      prev_slide = should_loop ? current_slide.prev() : slider.children('.slide:last');
+      prev_slide = should_loop
+        ? current_slide.prev()
+        : slider.children(".slide:last");
 
-    current_slide.removeClass('show');
-    prev_slide.addClass('show');
+    current_slide.removeClass("show");
+    prev_slide.addClass("show");
 
-    $('#indicator').html((prev_slide.index() + 1) + ' / ' + total_slides);
-
+    $("#indicator").html(prev_slide.index() + 1 + " / " + total_slides);
   });
-
 });
 
-$('.gallery').click(function () {
-  $('.gallery').hide();
-  $('.controller').addClass('off');
-  $('.toc-wrapper, footer, .nav-links').removeClass('blur');
+$(".gallery").click(function () {
+  $(".gallery").hide();
+  $(".controller").addClass("off");
+  $(".toc-wrapper, footer, .nav-links").removeClass("blur");
 });
 
-$('.controller').click(function () {
-  $('.gallery').show();
-  $('.controller').removeClass('off');
-  $('.toc-wrapper, footer, .nav-links').addClass('blur');
+$(".controller").click(function () {
+  $(".gallery").show();
+  $(".controller").removeClass("off");
+  $(".toc-wrapper, footer, .nav-links").addClass("blur");
 });
 
 $(".gallery > div:gt(0)").hide();
 
 setInterval(function () {
-  $('.gallery > div:first')
+  $(".gallery > div:first")
     .fadeOut(0)
     .next()
     .fadeIn(0)
     .end()
-<<<<<<< HEAD
-    .appendTo('.gallery');
-}, 2000);
-=======
     .appendTo(".gallery");
 }, 2100);
->>>>>>> bc294cefb2543755fd5e123f53404cd710a4f77c
 
-
-
-
-$('.shop>.header').click(function () {
+$(".shop>.header").click(function () {
   var expander = $(this).siblings(),
-      opensign = $('.opennow, .opensoon, .tickets, .watchnow');
+    opensign = $(".opennow, .opensoon, .tickets, .watchnow");
 
-  expander.toggleClass('show');
-  opensign.toggleClass('hide');
-
-  shop.prshopDefault();
+  expander.toggleClass("show");
+  opensign.toggleClass("hide");
 });
 
-$('#archive').click(function () {
-
-  $('.overlay').addClass('hide');
-  $('.shop>.toc-expander').addClass('show');
-
+$("#archive").click(function () {
+  $(".overlay").addClass("hide");
+  $(".shop>.toc-expander").addClass("show");
 });
-
 
 $(function () {
   var loc = window.location.href;
   if (/designers/.test(loc)) {
-    $('.toc-wrapper, footer, .nav-links').removeClass('blur');
+    $(".toc-wrapper, footer, .nav-links").removeClass("blur");
   }
   if (/info/.test(loc)) {
-    $('.toc-wrapper, footer, .nav-links').removeClass('blur');
+    $(".toc-wrapper, footer, .nav-links").removeClass("blur");
   }
 });
 
+$(".bio-expand-container>a").click(function () {
+  var expand = $(this).parent().children(".bio-expander");
 
-$('.bio-expand-container>a').click(function () {
-  
-  var expand = $(this).parent().children('.bio-expander');
+  expand.toggleClass("show");
+  $(this).toggleClass("active");
 
-  expand.toggleClass('show');
-  $(this).toggleClass('active');
-
-  
   if ($(".active")[0]) {
-    $('.bio-expand-container a').addClass('inactive');
-    $('.active').removeClass('inactive')
+    $(".bio-expand-container a").addClass("inactive");
+    $(".active").removeClass("inactive");
   } else {
-    $('.bio-expand-container a').removeClass('inactive');
+    $(".bio-expand-container a").removeClass("inactive");
   }
-
-});
-
-
-$(function () {
-  var open     = $('.shop[data-open="true"]'),
-      opensoon = $('.shop[data-opensoon="true"]'),
-      tickets = $('.shop[data-tickets="true"]'),
-      watchnow = $('.shop[data-watchnow="true"]');
-
-  open.children('.header').append('<div class="opennow">Open now</div>');
-  opensoon.children('.header').append('<div class="opensoon">Opening soon</div>');
-  tickets.children('.header').append('<div class="tickets">Tickets</div>');
-  watchnow.children('.header').append('<div class="watchnow">Watch now</div>');
-
-  
-
 });
 
 $(function () {
-  open  = $('.shop[data-event="true"]');
-  open.addClass('event');
-});
+  var open = $('.shop[data-open="true"]'),
+    opensoon = $('.shop[data-opensoon="true"]'),
+    tickets = $('.shop[data-tickets="true"]'),
+    watchnow = $('.shop[data-watchnow="true"]'),
+    event = $('.shop[data-event="true"]');
 
-$(function () {
-  $(".slider:empty, .slider:contains(' ')").parent().hide();
+  open.children(".header").append('<div class="opennow">Open now</div>');
+  opensoon.children(".header").append('<div class="opensoon">Opening soon</div>');
+  tickets.children(".header").append('<div class="tickets">Tickets</div>');
+  watchnow.children(".header").append('<div class="watchnow">Watch now</div>');
+  event.addClass("event");
 });
-
